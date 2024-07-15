3dd8fe20fc0895261a1c3dea6bba73faf1db903b
subhajit11010 <142614931+subhajit11010@users.noreply.github.com>
2024-07-15 17:16:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Message from './message';
+function App(){
+  return <div><Message/></div>;
+}
+export default App;
---
+++ src/message.jsx
@@ -0,0 +1,4 @@
+function Message(){
+    return <h1>dAPP</h1>;
+}
+export default Message;
